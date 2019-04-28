06a62a7df41ab9699edc08616622125269f553e7
Eyal Rozenberg <eyalroz@technion.ac.il>
2019-04-28 21:37:29 +0300

    Fixes #11: Almost all `string.h` functions implemented.

--- src/kat/on_device/miscellany.cuh
+++ src/kat/on_device/miscellany.cuh
@@ -16,40 +16,6 @@ __fd__  void swap(T& x, T& y) {
    y = _x;
 }
 
-/*
- ************************************************************************
- * C-standard-library-like functions available on the device
- ************************************************************************
- */
-
-// A single-thread inefficient version of strcmp
-__fd__  int strcmp_on_device(const char* src, const char* dst)
-{
-	int result = 0;
-
-	while (!(result = *(unsigned char *) src - *(unsigned char *) dst) && *dst)
-		++src, ++dst;
-
-	if (result < 0)
-		result = -1;
-	else if (result > 0) result = 1;
-
-	return result;
-}
-
-// A single-thread inefficient version of strcmp
-__fd__  char* strcpy_on_device(char *dst, const char *src)
-{
-	while (*dst != '\0') {
-		*(dst++) = *(src++);
-	}
-	*dst = *src;
-	return dst;
-}
-
-//
-//
-//
 /**
  * Copies some data from one location to antoher - using the native register
  * size for individual elements on CUDA GPUs, i.e. sizeof(int) = 4
@@ -130,39 +96,6 @@ void* long_memcpy(
 	case 3: *((char3*)destination_int) = *((const char3*)source_int); break;
 	}
 	return destination;
-}
-
-/**
- * Busy-sleep for (at least) a specified number of GPU cycles.
- *
- * @note GPU clock speed routinely varies significantly, and
- * scheduling is quite hard to predict, so do not expect
- * great accuracy w.r.t. the amount of _time_ this make your
- * warps sleep.
- *
- * @param[in] clock_cycle_ count minimum (and desired) number of
- * clock cycles to sleep.
- * @return the number of clock cycles actually spent busy-sleeping
- * (as well as can be measured)
- */
-inline __device__ clock_t busy_sleep(clock_t clock_cycle_count)
-{
-	clock_t start_clock = clock();
-	clock_t clock_offset = 0;
-	while (clock_offset < clock_cycle_count)
-	{
-		clock_offset = clock() - start_clock;
-	}
-	return clock_offset;
-}
-
-__fd__ void* memzero(void* destination, size_t size) { return memset(destination, 0, size); }
-
-template <typename T>
-constexpr __device__ __forceinline__
-typename std::enable_if<std::is_integral<T>::value, T>::type all_one_bits()
-{
-	return ~((T)0);
 }
 
 #include <kat/undefine_specifiers.hpp>
